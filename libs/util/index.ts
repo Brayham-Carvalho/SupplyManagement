a8f86a1e7a726e97da027a8ca6d0c858f69ccb1f
Brayham-Carvalho <chemicalbpc@gmail.com>
2024-07-11 11:23:47 -0300

    Commit ajuste de localizacao inicial no mapa

--- libs/util/index.ts
+++ libs/util/index.ts
@@ -1,8 +1,8 @@
 export const MAX_AGE = 1 * 24 * 60 * 60
 
 export const initialViewState = {
-  longitude: 80.2707,
-  latitude: 13.0827,
+  longitude: -40.30706,
+  latitude: -20.29812,
   zoom: 8,
 }
 
